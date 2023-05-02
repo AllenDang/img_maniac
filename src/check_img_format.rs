316c4bf99fa8181d2df34d2ba5e0cde9b8cc6692
Allen <allengnr@gmail.com>
2023-05-02 14:11:19 +0800

    Remove gif

--- src/check_img_format.rs
+++ src/check_img_format.rs
@@ -5,8 +5,9 @@ pub fn is_supported_format(pb: &Path) -> bool {
         if let Some(ext) = pb.extension() {
             if let Some(ext_str) = ext.to_str() {
                 match ext_str.to_lowercase().as_str() {
-                    "jpg" | "jpeg" | "png" | "gif" | "bmp" | "dds" | "exr" | "tga" | "ktx2"
-                    | "hdr" => return true,
+                    "jpg" | "jpeg" | "png" | "bmp" | "dds" | "exr" | "tga" | "ktx2" | "hdr" => {
+                        return true
+                    }
                     _ => return false,
                 }
             }
